b5e0ed00b5329fdefc1f3489188b50d7fb9d39c3
LoveCryGhost <andy.mytutor@gmail.com>
2020-03-25 21:54:36 +0800

    npm install

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,73 @@
-@extends('layouts.app')
+@extends(config('theme.user.user-app-login'))
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+@section('title','用戶-登入')
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+{{--清空--}}
+@section('app-content','')
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+@section('content-header')
+    <body class="hold-transition theme-yellow bg-img" data-overlay="3">
+    <div class="auth-2-outer row align-items-center h-p100 m-0">
+        <div class="auth-2">
+            <div class="auth-logo font-size-30">
+                <a href="/" class="text-dark">主 頁</a>
+            </div>
+            <!-- /.login-logo -->
+            <div class="auth-body">
+                <p class="auth-msg text-black-50">登入 - 可享會員福利</p>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                <form action="{{route('login')}}" method="post" class="form-element">
+                    @csrf
+                    <div class="form-group has-feedback">
+                        <input type="email" class="form-control" name="email" required placeholder="Email" value="{{ old('email')}}">
+                        <span class="ion ion-email form-control-feedback text-dark"></span>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-control" name="password"  required placeholder="Password">
+                        <span class="ion ion-locked form-control-feedback text-dark"></span>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="checkbox">
+                                <input type="checkbox"  name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label for="remember" class="text-dark">記住我</label>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <!-- /.col -->
+                        <div class="col-6">
+                            <div class="fog-pwd">
+                                <a href="{{route('password.request')}}"><i class="ion ion-locked"></i> 忘記密碼 ?</a><br>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                        <!-- /.col -->
+                        <div class="col-12 text-center pt-30">
+                            <button type="submit" class="btn btn my-20 btn-success">登入</button>
                         </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                <div class="text-center text-dark">
+                    <p class="mt-50">- 其他登入方式 -</p>
+                    <p class="gap-items-2 mb-20">
+                        <a class="btn btn-social-icon btn-round btn-facebook" href="#"><i class="fa fa-facebook"></i></a>
+                        <a class="btn btn-social-icon btn-round btn-twitter" href="#"><i class="fa fa-twitter"></i></a>
+                        <a class="btn btn-social-icon btn-round btn-google" href="#"><i class="fa fa-google"></i></a>
+                        <a class="btn btn-social-icon btn-round btn-instagram" href="#"><i class="fa fa-instagram"></i></a>
+                    </p>
                 </div>
+                <!-- /.social-auth-links -->
+
+                <div class="margin-top-30 text-center text-dark">
+                    <p>不是會員?   <a href="{{route('register')}}" class="text-info ml-20">註冊</a></p>
+                </div>
+
             </div>
         </div>
+
     </div>
-</div>
-@endsection
+@stop
+
+
+
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,54 @@
-@extends('layouts.app')
+@extends(config('theme.user.user-app'))
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@section('title','忘記-密碼')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+
+{{--清空--}}
+@section('app-content','忘記密碼')
+
+@section('content-header')
+    <body class="hold-transition theme-yellow bg-img" data-overlay="3">
+    <div class="auth-2-outer row align-items-center h-p100 m-0">
+        <div class="auth-2">
+            <div class="auth-logo font-size-30">
+                <a href="/" class="text-dark">主 頁</a>
+            </div>
+            <!-- /.login-logo -->
+            <div class="auth-body">
+                <p class="auth-msg text-black-50">重置密碼</p>
+                @if (session('status'))
+                    <div class="alert alert-success text-center" role="alert">
+                        {{ __('密碼重置郵件已經發送至您的指定信箱!') }}
+                    </div>
+                @endif
+
+                <form action="{{ route('password.email') }}" method="post" class="form-element">
+                    @csrf
+
+                    {{--驗證碼--}}
+                    <div class="form-group has-feedback">
+                        <input id="email" type="email" class="form-control" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                        <span class="ion ion-locked form-control-feedback text-dark"></span>
+                    </div>
+                    @if ($errors->has('captcha'))
+                        <span class="text-danger text-right" role="alert">
+                            <strong>{{ $errors->first('captcha') }}</strong>
+                        </span>
                     @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    <div class="row">
+                        <div class="col-12 text-center pt-30">
+                            <button type="submit" class="btn btn my-20 btn-success">重置密碼</button>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
+
     </div>
-</div>
-@endsection
+@stop
+
+
+
+
+
