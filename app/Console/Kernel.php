7a3cb99f036d9b945c13b25722d8feb8509b2482
LoveCryGhost <andy.mytutor@gmail.com>
2020-03-25 21:26:12 +0800

    config/database.php mysql=> ['strict' => false ...

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,35 +7,24 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
+
     protected $commands = [
-        //
+        //\App\Console\Commands\AddUsersCron::class,
+        \App\Console\Commands\CrawlerCleanCron::class,
+        \App\Console\Commands\CrawlerFirstTimeUpdateItemAndShopCron::class,
+        \App\Console\Commands\CrawlerTaskCron::class
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        //$schedule->command('command:add_users')->everyMinute();
+        $schedule->command('command:crawler_first_time_update_item_and_shop')->everyMinute();
+        $schedule->command('command:crawler_task')->everyMinute();
+        $schedule->command('command:crawler_clean')->everyThirtyMinutes();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
     }
 }
