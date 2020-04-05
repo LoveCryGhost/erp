c3e3eba94d758876e1ae36436d022c9f277e1a8a
LoveCryGhost <andy.mytutor@gmail.com>
2020-04-05 11:56:29 +0800

    可以顯示view

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -65,21 +65,24 @@ class ProductsTableSeeder extends Seeder
                     'c_ids' => [2],
                     'produuct_thumnail_ids' => ['/images/default/products/pizza_pan_1.jpg', '/images/default/products/pizza_pan_2.jpg'],
                     'skus' => [
-                        ['1', 'Pizza 7"烤盤', 100, 'sku_attributes' =>[ 1=>'黑色', 2=>'鐵氟龍', 3=>'7"']], //1 = member_id
-                        ['1', 'Pizza 8"烤盤',300, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 9"烤盤',400, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 10"烤盤',700, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 11"烤盤',900, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 12"烤盤',110, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 13"烤盤',130, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 14"烤盤',150, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 7"烤盤', 111, 'sku_attributes' =>[ 1=>'黑色', 2=>'鐵氟龍', 3=>'7"']], //1 = member_id
+                        ['1', 'Pizza 8"烤盤', 112, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 9"烤盤', 113, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 10"烤盤', 114, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 11"烤盤', 115, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 12"烤盤', 116, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 13"烤盤', 117, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 14"烤盤', 118, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
                     ]
                 ],[
                     'is_active' => 1, 'publish_at' => null, 'member_id' => 1,
                     'p_name' => "吐司烤盤", 't_id' => 1,
                     'c_ids' => [2],
                     'produuct_thumnail_ids' => ['/images/default/products/toast_pan_1.jpg', '/images/default/products/toast_pan_2.jpg', '/images/default/products/toast_pan_3.jpg'],
-                    'skus' => []
+                    'skus' => [
+                        ['1', '花嘴 1', 121, 'sku_attributes' =>[ 1=>'黑色', 2=>'鐵氟龍', 3=>'7"']], //1 = member_id
+                        ['1', '花嘴 2', 122, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                    ]
                 ]
             ];
 
@@ -165,21 +168,24 @@ class ProductsTableSeeder extends Seeder
                     'c_ids' => [2],
                     'produuct_thumnail_ids' => ['/images/default/products/pizza_pan_1.jpg', '/images/default/products/pizza_pan_2.jpg'],
                     'skus' => [
-                        ['1', 'Pizza 5-7"烤盤', 510, 'sku_attributes' =>[ 1=>'黑色', 2=>'鐵氟龍', 3=>'7"']], //1 = member_id
-                        ['1', 'Pizza 5-8"烤盤', 520, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 5-9"烤盤', 530, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 5-10"烤盤', 540, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 5-11"烤盤', 550, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 5-12"烤盤', 560, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 5-13"烤盤', 570, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
-                        ['1', 'Pizza 5-14"烤盤', 580, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 5-7"烤盤', 211, 'sku_attributes' =>[ 1=>'黑色', 2=>'鐵氟龍', 3=>'7"']], //1 = member_id
+                        ['1', 'Pizza 5-8"烤盤', 212, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 5-9"烤盤', 213, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 5-10"烤盤', 214, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 5-11"烤盤', 215, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 5-12"烤盤', 216, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 5-13"烤盤', 217, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                        ['1', 'Pizza 5-14"烤盤', 218, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
                     ]
                 ],[
                     'is_active' => 1, 'publish_at' => null, 'member_id' => 5,
                     'p_name' => "吐司烤盤", 't_id' => 1,
                     'c_ids' => [2],
                     'produuct_thumnail_ids' => ['/images/default/products/toast_pan_1.jpg', '/images/default/products/toast_pan_2.jpg', '/images/default/products/toast_pan_3.jpg'],
-                    'skus' => []
+                    'skus' => [
+                        ['1', '花嘴 1', 221, 'sku_attributes' =>[ 1=>'黑色', 2=>'鐵氟龍', 3=>'7"']], //1 = member_id
+                        ['1', '花嘴 2', 222, 'sku_attributes' =>[ 1=>'AA', 2=>'BB', 3=>'CC']],
+                    ]
                 ]
             ];
 
